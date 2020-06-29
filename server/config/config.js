6bf2cf40f2f8025487feb785553fdb19eb293095
Emmanuel <emmanuelguariato11@gmail.com>
2020-06-29 17:08:06 -0500

    Login normal sección 10

--- server/config/config.js
+++ server/config/config.js
@@ -10,13 +10,30 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ==========================
+// Vencimiento del Token
+// ==========================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ==========================
+// SEED de autenticación
+// ==========================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ==========================
 // Base de Datos
 // ==========================
 
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = process.env.MONGO_LC;
+    urlDB = process.env.MONGO_LC || 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
 }
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,10 +6,18 @@ const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
 
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
     //res.json('get Usuario LOCAL!!!');
+
+    // return res.json({
+    //     usuario: req.usuario,
+    //     nombre: req.usuario.nombre,
+    //     email: req.usuario.email,
+    // });
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -43,7 +51,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let body = req.body;
 
@@ -77,7 +85,7 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -102,7 +110,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     //res.json('delete Usuario');
 
 
